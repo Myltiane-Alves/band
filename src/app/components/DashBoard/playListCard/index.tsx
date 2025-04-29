9b4132bb52a766641d68d1e874906550c8bb5b70
Myltiane Alves <myltiane.aux@gmail.com>
2025-04-28 21:30:07 -0300

    feat: add CONTAINER PLAY LIST

--- src/app/components/DashBoard/playListCard/index.tsx
+++ src/app/components/DashBoard/playListCard/index.tsx
@@ -13,13 +13,13 @@ export default function PlayListCard({ url, title, description, tone }: CardProp
     return (
         <div className={styles.container}>
             <div className={styles.containerVideo}>
-
                 <iframe 
-                    src={url}
+                    src={url.replace('watch?v=', 'embed/')}
                     width="100%"
                     height="100%"
                     frameBorder="0"
-                    allow="encrypted-media"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
                 ></iframe>
             </div>
             <div className={styles.containerInfo}>
