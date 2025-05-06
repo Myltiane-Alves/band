b6017c234318e16f172abe6fe35959bd4c7d7984
Myltiane Alves <myltiane.aux@gmail.com>
2025-05-06 20:39:52 -0300

    feat: add finalizando PLAY LIST

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,13 +6,14 @@ import Link from "next/link";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Cipher from "../components/cipher";
 import DashBoardComponent from "../components/DashBoard";
+import SidebarComponent from "@/components/Sidebar/Sidebar";
 export default function DashBoard() {
   return (
     <div className={styles.container}>
       <main >
           {/* <h1>Dashboard</h1> */}
-          <Sidebar />
           <DashBoardComponent />
+          {/* <SidebarComponent /> */}
           {/* <Cipher /> */}
 
           
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,45 +1,82 @@
+'use client'
 import Link from "next/link";
-import { useState } from "react";
-import { FiHome, FiLogOut, FiMenu, FiSettings, FiUser } from "react-icons/fi";
+import { useState, useEffect } from "react";
+import { FiHome, FiLogOut, FiMenu, FiSettings, FiUser, FiX } from "react-icons/fi";
 import styles from '../../styles/sidebar.module.scss';
-
+import Image from "next/image";
+import image from "../../../public/assets/logo.png";
 export default function Sidebar() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(true);
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      const mobileBreakpoint = 767;
+      setIsMobile(window.innerWidth <= mobileBreakpoint);
+      
+      // Se NÃO for mobile, força a sidebar a ficar aberta
+      if (window.innerWidth > mobileBreakpoint) {
+        setIsOpen(true);
+      } else {
+        setIsOpen(false);
+      }
+    };
   
-    const toggleSidebar = () => setIsOpen(!isOpen);
-  
-    return (
-      <>
-        {/* Toggle Button for Mobile */}
-        <button className={styles.menuButton} onClick={toggleSidebar}>
-          <FiMenu size={24} />
-        </button>
-  
-        <aside className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
-          <div className={styles.logo}>MinhaLogo</div>
-  
-          <nav className={styles.nav}>
-            <Link href="/" className={styles.link}>
-              <FiHome className={styles.icon} />
-              <span>Início</span>
-            </Link>
-            <Link href="/perfil" className={styles.link}>
-              <FiUser className={styles.icon} />
-              <span>Perfil</span>
-            </Link>
-            <Link href="/configuracoes" className={styles.link}>
-              <FiSettings className={styles.icon} />
-              <span>Configurações</span>
-            </Link>
-          </nav>
-  
-          <div className={styles.bottom}>
-            <Link href="/logout" className={styles.link}>
-              <FiLogOut className={styles.icon} />
-              <span>Sair</span>
-            </Link>
+    checkIfMobile(); // Verifica no carregamento
+    window.addEventListener('resize', checkIfMobile);
+    
+    return () => window.removeEventListener('resize', checkIfMobile);
+  }, []);
+  return (
+    <>
+
+      <aside className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
+        <div className={styles.containerLogo}>
+          <div className={styles.logo}>
+
+            <Image 
+              src={image}
+              width={100}
+              // height={100}
+            />
           </div>
-        </aside>
-      </>
-    );
-  }
+         
+          {isMobile && (
+            <button 
+              className={styles.menuButton} 
+              onClick={toggleSidebar}
+              style={{ left: isOpen ? '240px' : '1rem' }}
+            >
+              {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
+            </button>
+          )}
+
+        </div>
+
+        <nav className={styles.nav}>
+          <Link href="/" className={styles.link} onClick={() => isMobile && setIsOpen(false)}>
+            <FiHome className={styles.icon} />
+            <span>Início</span>
+          </Link>
+          <Link href="/perfil" className={styles.link} onClick={() => isMobile && setIsOpen(false)}>
+            <FiUser className={styles.icon} />
+            <span>Perfil</span>
+          </Link>
+          <Link href="/configuracoes" className={styles.link} onClick={() => isMobile && setIsOpen(false)}>
+            <FiSettings className={styles.icon} />
+            <span>Configurações</span>
+          </Link>
+        </nav>
+
+        <div className={styles.bottom}>
+          <Link href="/logout" className={styles.link} onClick={() => isMobile && setIsOpen(false)}>
+            <FiLogOut className={styles.icon} />
+            <span>Sair</span>
+          </Link>
+        </div>
+      </aside>
+    </>
+  );
+}
